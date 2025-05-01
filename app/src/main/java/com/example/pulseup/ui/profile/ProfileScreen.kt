035abb274cb47a467cf67a7f4f9c524331887647
Caleb-Brunson-SC <caleb.brunson.professional@gmail.com>
2025-05-01 11:41:30 -0400

    Updated profile screen

--- app/src/main/java/com/example/pulseup/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/pulseup/ui/profile/ProfileScreen.kt
@@ -1,14 +1,36 @@
 package com.example.pulseup.ui.profile
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.DateRange
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.pulseup.PulseUpTopAppBar
 import com.example.pulseup.R
 import com.example.pulseup.ui.navigation.NavigationDestination
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
 
 /**
  * Companion object for the Profile Screen's Navigation Destination.
@@ -36,6 +58,35 @@ fun ProfileScreen(
             )
         }
     ) {
-
+        Column (
+            modifier = Modifier
+                .padding(start = 4.dp, top = 100.dp, end = 4.dp)
+                .fillMaxSize()
+        ) {
+            Row (
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                // user profile image
+                Image(
+                    painter = painterResource(R.drawable.gigachad),
+                    contentDescription = "giga chad",
+                    modifier = Modifier
+                        .size(75.dp)
+                        .clip(CircleShape)
+                )
+                // user name
+                Spacer(modifier = Modifier.width(6.dp))
+                Text(
+                    text = "Giga Chad",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 24.sp
+                )
+            }
+            Row {
+                Text(
+                    text = "This is giga chad's user profile description text segment."
+                )
+            }
+        }
     }
 }
